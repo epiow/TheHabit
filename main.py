4443fb61524462b2b35caf585424d7f36b62c862
megapx <mega@megaa.moe>
2024-04-10 18:40:34 +0800

    added database instance in main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import flet as ft
 import View
 from backendController import Login
+import Database as db
+
+database = db.Database('./JSON/data.json')
 
 if __name__ == "__main__":
     ft.app(target=View.loginWindow)
