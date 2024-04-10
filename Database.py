f8c2da9ae1b8a0a71b31832239b1206eda673fcc
epiow <fsangilan@gmail.com>
2024-04-10 21:03:47 +0800

    added count on activities context

--- Database.py
+++ Database.py
@@ -1,5 +1,7 @@
 import json
 import os
+import pandas as pd
+import numpy as np
 from datetime import datetime
 '''
 Script below appends new users to the user.json database
@@ -8,8 +10,10 @@ records while the username and password parameters will be used to authenticate 
 '''
 class Database:
     file_path = os.path.join('JSON', 'data.json')
+
     def __init__(self, file_path):
         self.file_path = file_path
+
     def load_data(self):
         if os.path.exists(self.file_path):
             with open(self.file_path, 'r') as file:
@@ -25,7 +29,7 @@ class Database:
         data = self.load_data()
         for user in data:
             if user["username"] == username:
-                print("duplicate found")
+                print("Duplicate found")
                 return
         new_user = {
             'username': username,
@@ -37,29 +41,38 @@ class Database:
 
     def create_activity(self, user_id, activity_name, time_set, time_elapsed):
         data = self.load_data()
+        today_date = datetime.today().strftime('%Y/%m/%d')
         user_found = False
         for user in data:
-            if(user_id == user["username"]):
+            if user_id == user["username"]:
                 user_found = True
                 activity_found = False
                 for activity in user["activities"]:
-                    if(activity_name in activity["name"]):
+                    if activity_name in activity["name"]:
                         activity_found = True
-                        new_activity_entry = {
-                            "date_performed": datetime.today().strftime('%Y/%m/%d'),
-                            "time_set": time_set,
-                            "time_elapsed": time_elapsed
-                        }
-                        activity["times_performed"].append(new_activity_entry)
+                        for entry in activity["times_performed"]:
+                            if entry["date_performed"] == today_date:
+                                entry["count"] += 1
+                                entry["time_elapsed"]
+                                break
+                        else:
+                            new_activity_entry = {
+                                "date_performed": today_date,
+                                "time_set": time_set,
+                                "time_elapsed": time_elapsed,
+                                "count": 1 
+                            }
+                            activity["times_performed"].append(new_activity_entry)
                         break
-                if(activity_found == False):
+                if not activity_found:
                     new_activity = {
                         "name": activity_name,
                         "times_performed": [
                             {
-                                "date_performed": datetime.today().strftime('%Y/%m/%d'),
+                                "date_performed": today_date,
                                 "time_set": time_set,
-                                "time_elapsed": time_elapsed
+                                "time_elapsed": time_elapsed,
+                                "count": 1  
                             }
                         ]
                     }
--- usertest.py
+++ usertest.py
@@ -2,8 +2,19 @@ import Database
 
 db = Database.Database('./JSON/data.json')
 
+
 db.register_user('yo', 'yuh')
+db.register_user('Nhi', 'Gyro')
+db.create_activity('yo', 'Jogging', 3700, 1000)
+db.create_activity('yo', 'Skating', 3700, 1000)
+db.create_activity('yo', 'Parkouring', 3700, 1000)
+db.create_activity('yo', 'Reading', 3700, 1000)
+db.create_activity('Nhi', 'Parkouring', 3700, 1000)
 db.create_activity('yo', 'Jogging', 3700, 1000)
 db.create_activity('yo', 'Jogging', 3700, 1000)
-db.create_activity('yo', 'Reading', 3700, 1000)
-db.create_activity('yo', 'Reading', 3700, 1000)
+db.create_activity('Nhi', 'Reading', 3700, 1000)
+db.create_activity('Nhi', 'Reading', 3700, 1000)
+db.create_activity('Nhi', 'Parkouring', 3700, 1000)
+db.register_user('John Doe', 'johnnyboy')
+db.create_activity('John Doe', 'Jogging', 6969, 420)
+
