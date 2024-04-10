caba49dfd1bbd19f76c82a35c60e31833188ebab
megapx <mega@megaa.moe>
2024-04-10 21:04:04 +0800

    declared class equivalents of JSON file

--- backendController.py
+++ backendController.py
@@ -11,14 +11,14 @@ class User():
         self.username = username
         self.password = password
         self.activities = []
-
     def setActivities(self):
         data = self.io.load_data()
         for user in data:
             if(self.username == user["username"]):
-                print(user["activities"])
-                self.activities = user["activities"]
-
+                for activity in user["activities"]:
+                    activity_to_set = Activity(activity["name"], activity["times_performed"])
+                    self.activities.append(activity_to_set)
+                    
     def loginUser(self):
         data = self.io.load_data()
         userExists = False
@@ -33,13 +33,25 @@ class User():
         return self.db.create_user(self.username, self.password)
 
 class Activity():
-    def __init__(self, activity_name):
+    def __init__(self, activity_name, times_performed):
         self.name = activity_name
         self.entries = []
+        self.setEntries(times_performed)
+    def setEntries(self, times_performed):
+        for entry in times_performed:
+            new_entry = ActivityEntry(entry["date_performed"], entry["time_set"], entry["time_elapsed"], 0)
+            self.entries.append(new_entry)
+        
 class ActivityEntry():
     def __init__(self, date_performed, time_set, time_elapsed, count):
         self.date_performed = date_performed
         self.time_set = time_set
         self.time_elapsed = time_elapsed
         self.count = count
-    
+
+user = User("yo", "yuh")
+user.loginUser()
+for i in user.activities:
+    print(i.name)
+    for j in i.entries:
+        print(j.date_performed)
