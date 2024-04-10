b7c528931064af5d4c4ed10817cae2aa70682cc0
megapx <mega@megaa.moe>
2024-04-10 19:23:09 +0800

    rewrite certain backend classes

--- backendController.py
+++ backendController.py
@@ -1,5 +1,6 @@
 import flet as ft
 import os
+import DataIO
 import Database
 import json
 
@@ -7,64 +8,57 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
+        self.io = DataIO.DataIO('./JSON/data.json')
     def setHabits(self):
-        folder_path = 'JSON'
-        file_path_acts = os.path.join(folder_path, 'activities.json')
-        with open(file_path_acts, 'r') as acts:
-            activities = json.load(acts)
-        
-            for activity in activities:
-                if self.userID == int(activity['user_id']):
-                    habit = Habit(activity['activity'], activity['activity_kind'], activity["done"])   
+        data = self.io.load_data()
+        for user in data:
+            if(self.username == user["username"]):
+                for activity in user["activities"]:
+                    habit = Habit(activity["name"], activity["time_set"])   
                     print(self.username + ' ' + habit.habit_name)
 class Login(User):
     def __init__(self, username, password):
         super().__init__(username, password)
-
+        self.io = DataIO.DataIO('.JSON/data.json')
+        
     def userVerification(self):
-        folder_path = 'JSON'
-        file_path_user = os.path.join(folder_path, 'user.json')
-        try:
-            with open(file_path_user, 'r') as file:
-                user_data = json.load(file)
-        except FileNotFoundError:
-            print("Error: File 'user.json' not found")
-            return False
-        except json.JSONDecodeError:
-            print("Error: Failed to decode JSON from 'users.json'")
-            return False
+        data = self.io.load_data()
+    #    try:
+    #        with open(file_path_user, 'r') as file:
+    #            user_data = json.load(file)
+    #    except FileNotFoundError:
+    #        print("Error: File 'user.json' not found")
+    #        return False
+    #    except json.JSONDecodeError:
+    #        print("Error: Failed to decode JSON from 'users.json'")
+    #        return False
 
-        for users in user_data:
-            if self.username == users['username'] and self.password == users['password']:
-                self.userID = users['id']
+        for user in data:
+            if self.username == user["username"] and self.password == user["password"]:
                 super().setHabits()
                 return True
+            else: 
+                return False
         #if self.username in user_data and user_data["password"] == self.password:
         #    self.authenticated = True
         #    return True
-            else:
-                return False
- 
 class Register:
-    def __init__(self, username, password):
-        if not self.alreadyRegistered(username, password):
-            Database.register_new_user(username, password, './JSON/user.json')
+    #register function
+    def __init__ (self, username, password):
+        self.username = username
+        self.password = password
 
-    def alreadyRegistered(self, username, password):
-        folder_path = 'JSON'
-        file_path_user = os.path.join(folder_path, 'user.json')
-
-        with open(file_path_user, 'r') as file:
-            user_data = json.load(file)
-
-        for users in user_data:
-            if username == users['username']:
-                return True
-        return False
+    def registerUser(self, user_data):
+        if self.username in user_data:
+            return False
+        else:
+            user_data[self.username] = self.password
+            with open('user.json', 'w') as file:
+                json.dump(user_data, file)
+            return True 
         
 class Habit(User):
-    def __init__(self, habit_name, habit_description, habit_setting):
+    def __init__(self, habit_name, habit_setting):
         self.habit_name = habit_name
         self.habit_description = habit_description
         self.habit_TimeSetter(habit_setting)
@@ -72,12 +66,13 @@ class Habit(User):
     def habit_TimeSetter(self, habit_setting):
         for x in habit_setting:
             dayOfActivity = Calendar(x[0], x[1])
-
-    def set_newHabit():
-        pass
 class Calendar(Habit):
     def __init__(self, habit_date, habit_day):
         #self.specific_habit = habit_name
         self.habit_date = habit_date
         self.habit_day = habit_day
         self.calendar = { "Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [], "Saturday": [], "Sunday": [] }
+
+
+login = Login("fp", "sangilan")
+login.userVerification()
