f4315871ae0b0659bd7b552eee2dadc092ef0e29
megapx <mega@megaa.moe>
2024-04-10 22:22:46 +0800

    updated usertest script

--- usertest.py
+++ usertest.py
@@ -1,10 +1,10 @@
 import Database
+from DataIO import DataIO
+io = DataIO('./JSON/data.json')
+db = Database.Database(io)
 
-db = Database.Database('./JSON/data.json')
-
-
-db.register_user('yo', 'yuh')
-db.register_user('Nhi', 'Gyro')
+db.create_user('yo', 'yuh')
+db.create_user('Nhi', 'Gyro')
 db.create_activity('yo', 'Jogging', 3700, 1000)
 db.create_activity('yo', 'Skating', 3700, 1000)
 db.create_activity('yo', 'Parkouring', 3700, 1000)
@@ -15,6 +15,6 @@ db.create_activity('yo', 'Jogging', 3700, 1000)
 db.create_activity('Nhi', 'Reading', 3700, 1000)
 db.create_activity('Nhi', 'Reading', 3700, 1000)
 db.create_activity('Nhi', 'Parkouring', 3700, 1000)
-db.register_user('John Doe', 'johnnyboy')
+db.create_user('John Doe', 'johnnyboy')
 db.create_activity('John Doe', 'Jogging', 6969, 420)
 
