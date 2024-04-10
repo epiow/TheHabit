05fd6857345ff719291903d8d5f6fd2f1628ed4b
megapx <mega@megaa.moe>
2024-04-10 19:37:49 +0800

    early timestamp tests

--- Database.py
+++ Database.py
@@ -39,7 +39,7 @@ class Database:
                     if(activity_name in activity["name"]):
                         activity_found = True
                         new_activity_entry = {
-                            "date_performed": datetime.today().strftime('%Y/%m/%d'),
+                            "date_performed": datetime.today().timestamp(),
                             "time_set": time_set,
                             "time_elapsed": time_elapsed
                         }
@@ -50,7 +50,7 @@ class Database:
                         "name": activity_name,
                         "times_performed": [
                             {
-                                "date_performed": datetime.today().strftime('%Y/%m/%d'),
+                                "date_performed": datetime.today().timestamp(),
                                 "time_set": time_set,
                                 "time_elapsed": time_elapsed
                             }
