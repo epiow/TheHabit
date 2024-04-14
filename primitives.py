287b67ed76c58557e5293c8cb0767f6f36d9144f
megapx <mega@megaa.moe>
2024-04-14 10:39:05 +0800

    improved global variable object

--- primitives.py
+++ primitives.py
@@ -1,20 +1,31 @@
 import flet as ft
 import os
+
 def yo():
     print("yo")
+def set_transparency(value: str, alpha):
+    return ('#' + hex(int(alpha * 255)).lstrip("0x") + value.lstrip("#")).upper()
 
-UserVariables = {
-    "window.scale": 2,
+class UserColors:
+    accent      = '#1BCF6E'
+    background  = '#E5E5E5'
+    foreground  = '#000000'
 
-    "color.accent": "#1bcf6e",
-    "color.background": "#e5e5e5",
-    "color.dotmatrix": "#0D000000",
-    "color.foreground": "#D9000000",
-}
-
-def scale(value):
-    return value * UserVariables["window.scale"]
-
+class UserProperties:
+    scale_factor        = 2
+    default_text_style  = ft.TextStyle(
+                            font_family = 'Rockwell',
+                            weight      = '400',
+                            size        = 12 * scale_factor,
+                        )
+    textfield_style     = ft.TextStyle(
+                            font_family = 'Rockwell',
+                            weight      = '400',
+                            size        = 12 * scale_factor,
+                            color       = UserColors.foreground
+                        )
+    def scale(self,value):
+        return value * self.scale_factor
 def login(page: ft.Page):
     vars = UserVariables
     page.title = "Login Example"
