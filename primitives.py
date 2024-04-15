b300ceea0cb77ddd580cbef90dbc51efda7b9979
megapx <mega@megaa.moe>
2024-04-15 20:47:48 +0800

    fixed filenames

--- primitives.py
+++ primitives.py
@@ -1,9 +1,9 @@
 import flet as ft
-
 class UserColorsLight:
     accent      = '#1BCF6E'
     background  = '#E5E5E5'
     foreground  = '#1D2833'
+    
 class UserColorsDark:
     accent      = '#CE1D6E'
     foreground  = '#E5E5E5'
@@ -24,12 +24,13 @@ class UserProperties:
                             size        = 12 * scale_factor,
                             color       = colors.foreground
                         )
-    logo_big_full       = 'logo-big-full-dark.svg'
-    dots                = 'dots-dark.svg'
+    logo_big_full       = 'logo-big-full-light.svg'
+    dots_login          = 'dots-login-light.svg'
     def scale(self,value):
         return value * self.scale_factor
     def set_transparency(self, value: str, alpha: float):
         return ('#' + hex(int(alpha * 255)).lstrip("0x") + value.lstrip("#")).upper()
+
 class UserControls:
     def Icon(src, fit, width, height):
         return ft.Image(
