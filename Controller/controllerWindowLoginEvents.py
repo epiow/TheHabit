790b9d16594bc003fbdd28fec119a9fee5f70b89
megapx <mega@megaa.moe>
2024-05-01 17:09:24 +0800

    began routing

--- Controller/controllerWindowLoginEvents.py
+++ Controller/controllerWindowLoginEvents.py
@@ -1,9 +1,16 @@
-import flet as ft
+from View.viewProperties import *
+from View.viewWindowMain import *
 
 
 def eventButtonLoginClick(e: ft.ControlEvent):
-    f: ft.Page = e.control.data
-    f.window_close()
-    
+    page: ft.Page = e.page
+    vars: UserProperties = page.data
+    colors = vars.colors
+    test = windowMain(page)
+    view = ft.View(controls=[test.stack], bgcolor=colors.background)
+    page.views.clear()
+    page.views.append(view)
+    page.update()
+
 def eventButtonLoginHover(e):
     pass
--- View/viewWindowMain.py
+++ View/viewWindowMain.py
@@ -1,12 +1,12 @@
 from View.viewProperties import *
-from Controller.controllerWindowMainEvents import *
-vars = UserProperties()
-colors = vars.colors
-class testMain:
-    
-    def __init__(self, page: ft.Page):
-        self.vars = UserProperties()
-        self.colors = self.vars.colors
+class windowMain:
+
+    vars = UserProperties()
+    colors = vars.colors
+    def __init__(self, page):
+        from Controller.controllerWindowMainEvents import eventToggleCrossOnClick, eventButtonOnHover
+        vars = self.vars
+        colors = self.colors
         page.title              = "the Habit: Login"
         page.window_width       = vars.scale(484)
         page.window_height      = vars.scale(396)
@@ -19,7 +19,7 @@ class testMain:
         page.window_title_bar_buttons_hidden = False
 
 
-        bgDots = lambda: ft.Image(
+        bgDots = ft.Image(
             src=os.path.join(os.getcwd(), "Assets", 'dots-main-light.svg'),
             fit = ft.ImageFit.FILL,
             width = page.window_width,
@@ -54,7 +54,7 @@ class testMain:
             color=colors.foreground
         )
         bgDots = ft.Container(
-            bgDots(),
+            bgDots,
             opacity = 0.2,
             left=0,
             top=0
@@ -118,126 +118,5 @@ class testMain:
                 staticWelcomeUser
             ] + listCrosses
         )
-        self.page = page
-    def getpage(self):
-        return self.page
-    def build(self):
-        return ft.View(route='main', controls=[self.stack])
         #page.add(stack)
-def windowMain(page: ft.Page):
-    vars = UserProperties()
-    colors = vars.colors
-
-    page.title              = "the Habit: Login"
-    page.window_width       = vars.scale(484)
-    page.window_height      = vars.scale(396)
-    page.bgcolor            = colors.background
-    page.window_resizable   = False
     
-    
-    #if page.platform is ft.PagePlatform.MACOS:
-    #   page.window_title_bar_hidden = False
-    page.window_title_bar_buttons_hidden = False
-
-
-    bgDots = ft.Image(
-        src=os.path.join(os.getcwd(), "Assets", 'dots-main-light.svg'),
-        fit = ft.ImageFit.FILL,
-        width = page.window_width,
-        height = page.window_height
-    )
-    iconLeftArrow = lambda: ft.Image(
-        src=os.path.join(os.getcwd(), "Assets", 'icon-left-arrow-light.svg'),
-        fit = ft.ImageFit.FILL,
-        width=vars.scale(22),
-        height=vars.scale(22)
-    )
-    iconRightArrow = lambda: ft.Image(
-        src=os.path.join(os.getcwd(), "Assets", 'icon-right-arrow-light.svg'),
-        fit = ft.ImageFit.FILL,
-        width=vars.scale(22),
-        height=vars.scale(22)
-    )
-    iconCross = lambda: ft.Image(
-        src=os.path.join(os.getcwd(), "Assets", 'icon-cross-light.svg'),
-        fit=ft.ImageFit.FILL,
-        width = vars.scale(22),
-        height= vars.scale(22)
-    )
-    staticDatePicker = ft.Text(
-        "2024, April",
-        style=vars.default_text_style,
-        color=colors.foreground
-    )
-    staticWelcomeUser = ft.Text(
-        "Welcome, User!",
-        style=vars.default_text_style,
-        color=colors.foreground
-    )
-    bgDots = ft.Container(
-        bgDots,
-        opacity = 0.2,
-        left=0,
-        top=0
-    )
-    buttonLeftArrow = ft.Container(
-        iconLeftArrow(),
-        left=vars.scale(308),
-        top=vars.scale(66),
-        on_hover=eventButtonOnHover,
-    )
-    buttonRightArrow = ft.Container(
-        iconRightArrow(),
-        left=vars.scale(440),
-        top=vars.scale(66),
-        data=page,
-        on_hover=eventButtonOnHover,
-    )
-    toggleCross = lambda: ft.Container(
-        iconCross(),
-        data=True,
-        left=vars.scale(154),
-        top=vars.scale(154),
-        on_hover=eventButtonOnHover,
-        on_click=eventToggleCrossOnClick
-    )
-    staticDatePicker = ft.Container(
-        staticDatePicker,
-        padding=ft.padding.only(top=6),
-        width=vars.scale(110),
-        height=vars.scale(22),
-        left=vars.scale(330),
-        top=vars.scale(66),
-        alignment=ft.alignment.center
-    )
-    staticWelcomeUser = ft.Container(
-        staticWelcomeUser,
-        padding=ft.padding.only(right=6, top=6),
-        width=vars.scale(154),
-        height=vars.scale(22),
-        left=vars.scale(308),
-        top=vars.scale(44),
-        alignment=ft.alignment.center_right
-
-    )
-    def crosses(count):
-        items = []
-        offset = 0
-        for i in range(1, count + 1):
-            newToggleCross = toggleCross()
-            newToggleCross.left += vars.scale(offset)
-            items.append(newToggleCross)
-            offset += 22
-        return items
-    listCrosses = crosses(14)
-    stack = ft.Stack(
-        [
-            bgDots,
-            buttonLeftArrow,
-            buttonRightArrow,
-            staticDatePicker,
-            staticWelcomeUser
-        ] + listCrosses
-    )
-    return ft.View(route='main', controls=[stack])
-    #page.add(stack)
